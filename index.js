264ab691ec4a9d58a5bfd934f35fcce373e64422
Aleksey Lisun <insuline.ru@gmail.com>
2023-02-12 20:55:31 +0300

    :sparkles: feat: Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,7 @@ const api = new ChatGPTAPI({
   apiKey,
 })
 
-async function main() {
-  const diff = execSync('git diff --staged').toString()
-
-  // Handle empty diff
-  if (!diff) {
-    console.log('No changes to commit 🙅');
-    console.log('May be you forgot to add the files? Try `git add .` and then run this script again.')
-    process.exit(1);
-  }
-
+const generateSingleCommit = async () => {
   const prompt = 'I want you to act as a commit message generator. I will provide you with my code changes as a git diff and I would like you to generate an appropriate commit message. Try to understand the meaning of the changes, not just the name of the file. In our project, we use conventional commits and gitmoji to design the messages. The commit structure should be of `<emoji> <type in lowercase>: <subject>`\nHere is a list of changes:\n'
 
   const { text } = await api.sendMessage(prompt + diff)
@@ -51,4 +42,38 @@ async function main() {
     });
 }
 
-main();
+const generateListCommits = async (diff) => {
+  const prompt = 'I want you to act as a commit message generator. I will provide you with my code changes as a git diff and I would like you to generate an appropriate commit message. Generate 5 variants and send them in one line, separated by commas. Try to understand the meaning of the changes, not just the name of the file. In our project, we use conventional commits and gitmoji to design the messages. The commit structure should be of `<emoji> <type in lowercase>: <subject>`\nHere is a list of changes:\n'
+
+  const { text } = await api.sendMessage(prompt + diff)
+
+  const msgs = text.split(',').map((msg) => msg.trim())
+
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'commit',
+      message: 'Select a commit message',
+      choices: msgs,
+    },
+  ]).then((answers) => {
+    console.log('Committing Message... 🚀 ')
+    execSync(`git commit -F -`, { input: answers.commit });
+    console.log('Commit Successful! 🎉')
+  });
+}
+
+async function generateAICommit() {
+  const diff = execSync('git diff --staged').toString()
+
+  // Handle empty diff
+  if (!diff) {
+    console.log('No changes to commit 🙅');
+    console.log('May be you forgot to add the files? Try `git add .` and then run this script again.')
+    process.exit(1);
+  }
+
+  args.list ? await generateListCommits(diff) : await generateSingleCommit(diff)
+}
+
+generateAICommit();
