e2da127acec3509f8a025ed754e75e793ba42ad8
evgenyatlas <sacraka@gmail.com>
2023-03-02 00:26:11 +0300

    👷 fix: empty arguments in getArgs()

--- helpers.js
+++ helpers.js
@@ -1,17 +1,20 @@
 const getArgs = () => {
-  return process.argv.slice(2)
-    .reduce((args, arg) => {
-      if (arg.startsWith('--')) {
-        const [flag, value = true] = arg.split('=');
-        args[flag.slice(2)] = value;
-      } else if (arg[0] === '-') {
-        arg.slice(1).split('').forEach(flag => {
-          args[flag] = true;
-        });
-      }
+  const args = process.argv.slice(2);
+  const result = {};
 
-      return args;
-    }, {});
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    const key = arg.replace(/^--/, '');
+    const nextArg = args[i + 1];
+    if (/^--/.test(nextArg) || nextArg === undefined) {
+      result[key] = true;
+    } else {
+      result[key] = nextArg;
+      i++;
+    }
+  }
+
+  return result;
 };
 
 export { getArgs }
