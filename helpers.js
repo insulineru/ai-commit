452357bc0ee2d8e2a4aa3b38c4cdb7c2275e7851
Aleksey Lisun <insuline.ru@gmail.com>
2023-03-06 23:10:40 +0700

    ✨ feat: add checkGitRepository function to helpers.js (#7)
    This commit adds the `checkGitRepository` function to the `helpers.js` module which checks if the current directory is a Git repository using the `git rev-parse --is-inside-work-tree` command. If it's not a Git repository, an error message will be logged to the console and the process will exit with code 1. The `checkGitRepository` function is exported from the module along with the `getArgs` function.
    
    Additionally, this commit modifies the `index.js` module to import `checkGitRepository` from `helpers.js`. The `generateAICommit` function now calls `checkGitRepository` before executing the `git diff --staged` command and logs an error message if it returns false.

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+import { execSync } from "child_process";
+
 const getArgs = () => {
   const args = process.argv.slice(2);
   const result = {};
@@ -17,4 +19,13 @@ const getArgs = () => {
   return result;
 };
 
-export { getArgs }
+const checkGitRepository = () => {
+  try {
+    const output = execSync('git rev-parse --is-inside-work-tree', { encoding: 'utf-8' });
+    return output.trim() === 'true';
+  } catch (err) {
+    return false;
+  }
+};
+
+export { getArgs, checkGitRepository }
