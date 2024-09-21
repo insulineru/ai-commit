e6d33e1a2e0c6a01232384afa0a525b8e92a51e1
HamduSalih <afa.hamdu@gmail.com>
2024-09-21 06:20:18 +0000

    Update instructions in the prompt to clarify that commit type should be specified where necessary.

--- openai.js
+++ openai.js
@@ -30,7 +30,7 @@ const openai = {
       "I want you to act as the author of a commit message in git." +
       `I'll enter a git diff, and your job is to convert it into a useful commit message in ${language} language` +
       (commitType ? ` with commit type '${commitType}'. ` : ". ") +
-      "Do not preface the commit with anything, use the present tense, return the full sentence: " +
+      "Do not preface the commit with anything, use the present tense, return the full sentence. Please specify commit type where necessary " +
       '\n\n'+
       diff
     );
@@ -42,7 +42,7 @@ const openai = {
       `I'll enter a git diff, and your job is to convert it into a useful commit message in ${language} language` +
       (commitType ? ` with commit type '${commitType}.', ` : ", ") +
       `and make ${numOptions} options that are separated by ";".` +
-      "For each option, use the present tense, return the full sentence" +
+      "For each option, use the present tense, return the full sentence. Please specify commit type where necessary." +
       diff;
 
     return prompt;
